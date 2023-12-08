8b82781184b845b8a7777f15b1d73da7a3424d8c
mrfukui <mr.fukui@yahoo.com>
2023-12-07 19:04:02 -0800

    Added login and signup components and added authentication token to MainView component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -36,17 +38,6 @@ export const MainView = () => {
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
-  if (!user) {
-    return (
-      <LoginView
-        onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }}
-      />
-    );
-  }
-
   useEffect(() => {
     if (!token) {
       return;
@@ -71,6 +62,21 @@ export const MainView = () => {
         setMovies(moviesFromApi);
       });
   }, [token]);
+
+  if (!user) {
+    return (
+      <>
+      <LoginView
+        onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }}
+      />
+      or
+      <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
